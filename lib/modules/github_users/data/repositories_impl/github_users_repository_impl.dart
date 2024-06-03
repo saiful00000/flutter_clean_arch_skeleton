19f5e9599ac26ae7bd7d99993f9f4e494f565660
shaiful islam <md.shaiful@apsissolutions.com>
2024-06-04 00:37:11 +0600

    Github api integration ongoing

--- lib/modules/github_users/data/repositories_impl/github_users_repository_impl.dart
+++ lib/modules/github_users/data/repositories_impl/github_users_repository_impl.dart
@@ -1,13 +1,36 @@
+import 'dart:developer';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter_clean_skeleton/infrastructure/network/api_urls.dart';
+
 import '../../business/entity/github_user.dart';
 import '../../business/repositories/github_users_repository.dart';
+import 'package:http/http.dart' as http;
 
 class GithubUsersRepositoryImpl implements GitHubUsersRepository {
 
-  GithubUsersRepositoryImpl();
-
   @override
-  Future<List<GithubUser>> getUsers() {
-    // TODO: implement getUsers
-    throw UnimplementedError();
+  Future<List<GithubUser>> getUsers() async {
+    try {
+      final uri = Uri.parse(ApiUrls.baseUrl + ApiUrls.usersUrl);
+      final headers = {'Authorization': 'Bearer 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'};
+
+      final response = await http.get(uri, headers: headers);
+
+      log('''URL      -> $uri
+      STATUS   -> ${response.statusCode}
+      LOAD     -> null
+      RESPONSE -> ${response.body}
+      ''');
+
+      if (response.statusCode == 200) {
+
+      }
+    } catch (error, stck) {
+      debugPrint(error.toString());
+      debugPrint(stck.toString());
+    }
+
+    return [];
   }
 }
